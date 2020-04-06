aa46f1bfbea47e28f3bde5d79732275a0affed93
Diane Crame <dcrame@icloud.com>
2020-04-06 21:21:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,9 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    artist_name, song_name, extra = filename.split(" - ")
-    song = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
-    artist.add_song(song)
+    song = self.new
+    song.name = filename.split(" - ")[1]
+    song
   end
 
 end
