098b93401aaac5661e94652e492b9798e8b9dea2
Diane Crame <dcrame@icloud.com>
2020-04-06 21:10:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,11 +21,7 @@ class Artist
   end
   
   def self.find_or_create_by_name(artist_name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
+    @@all.each.find {|name| name == artist_name}
   end
   
   def print_songs
