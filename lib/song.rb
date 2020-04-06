9fe3c0c3eb86739a10e21657d119bf0dfb148b20
Diane Crame <dcrame@icloud.com>
2020-04-06 22:13:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,11 +17,10 @@ class Song
     song_name = file.split(" - ")[1]
     song = Song.new(song_name)
     song.artist_name = artist_name
-    @@all << song
     song
   end
 
   def artist_name(artist_name)
-    
+    self.new = 
   end
 end
